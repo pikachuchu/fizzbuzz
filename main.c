1f446596443c54c7cc70ed38967788d90bbeee88
Chuchu Ma <sarahma88@gmail.com>
2015-03-20 00:29:19 -0500

    recalculated in terms of index inside loop

--- main.c
+++ main.c
@@ -1,27 +1,28 @@
 // @author Chuchu Ma, William Morriss
 #include <unistd.h>
 int main() {
-    long buffer[52], *curr = buffer, i = 0, base = 9;
-    *curr++ = 0x7A7A69460A320A31;
-    *curr++ = 0x0A7A7A75420A340A;
-    *curr++ = 0x380A370A7A7A6946;
-    *curr++ = 0x00000A7A7A69460A;
-    curr = (long*)((short *)curr - 1);
+    long a[52], *b = a, c = 0, d, e, f, g;
+    *b++ = 0x7A7A69460A320A31;
+    *b++ = 0xA7A7A75420A340A;
+    *b++ = 0x380A370A7A7A6946;
+    *b++ = 0xA7A7A69460A;
+    b = (long*)((short *)b - 1);
     loop:
-        base += 1;
-        *curr++ = 0x0A30300A7A7A7542 + (++base / 10 + (base++ % 10 << 8) << 40);
-        *curr++ = 0x0A30300A7A7A6946 + (++base / 10 + (base++ % 10 << 8) << 40);
-        *curr++ = 0x427A7A69460A3030 + (base / 10 + (base++ % 10 << 8));
-        *curr++ = 0x300A30300A7A7A75 + (((++base + 1) / 10 << 24) 
-                                   + base / 10 + (base++ % 10 << 8) << 32);
-        *curr++ = 0x300A7A7A69460A30 + base % 10 + ((base++ + 2) / 10 << 56);
-        *curr++ = 0x460A7A7A75420A30 + ++base % 10;
-        base += 2;
-        *curr++ = 0x300A30300A7A7A69 + (((++base + 1) / 10 << 24) 
-                                   + base / 10 + (base++ % 10 << 8) << 32);
-        *curr++ = 0x000A7A7A69460A30 + base++ % 10;
-        curr = (long*)((char *)curr - 1);
-        if (i ++< 5) goto loop;
-    *curr++ = 0x0000000A7A7A7542;
-    return write(1, buffer, 413) - 413;
+        //TODO: find relationship between d and f and between e and g
+        *b++ = 0xA31310A7A7A7542 + ((d = (((c > 1) << (c > 3)) + c)) 
+                                 + (e = ((c & 1) + ((c & 1) << 2) << 8)) << 40);
+        *b++ = 0xA33310A7A7A6946 + (d + e << 40);
+        *b++ = 0x427A7A69460A3431 + d + e;
+        *b++ = 0x310A31310A7A7A75 + ((f = (((c > 0) << (c > 2)) + c + (c == 5)))
+                                  + ((g = (!(c & 1) + (!(c & 1) << 2))) << 8)
+                                  + (f << 24) << 32);
+        *b++ = 0x310A7A7A69460A32 + g + (f << 56);
+        *b++ = 0x460A7A7A75420A34 + g;
+        *b++ = 0x320A32320A7A7A69 + ((d << 24) + d + e << 32);
+        *b++ = 0xA7A7A69460A33 + (e >> 8);
+        b = (long*)((char *)b - 1);
+        if (c ++< 5) goto loop;
+    *b = 0xA7A7A7542;
+    return write(1, a, 413) - 413;
 }
+
